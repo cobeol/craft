1f7cf04bd66a0b11f7353aef4b25c8dbe34dd3f2
코벌 <cobeol11111010111@gmail.com>
2024-12-31 07:08:47 +0900

    ⚡ 'EntityEventManager' 성능 최적화..?

--- craft-api/src/main/kotlin/io/github/cobeol/craft/event/EntityEventManager.kt
+++ craft-api/src/main/kotlin/io/github/cobeol/craft/event/EntityEventManager.kt
@@ -31,6 +31,8 @@ class EntityEventManager(private val plugin: Plugin) {
     private val providers = ConcurrentHashMap<KClass<out EntityProvider<*>>, EntityProvider<*>>()
     private val listeners = ConcurrentHashMap<Entity, MutableSet<Listener>>()
 
+    private val _providers = ConcurrentHashMap<Entity, EntityProvider<Event>>()
+
     /**
      * 해당 [Entity]의 전용 이벤트를 등록합니다.
      *
@@ -39,46 +41,56 @@ class EntityEventManager(private val plugin: Plugin) {
      */
     @Suppress("UNCHECKED_CAST")
     fun registerEvents(entity: Entity, listener: Listener) {
-        listener::class.members.filter { it.hasAnnotation<EventHandler>() }
-            .forEach { method ->
-                val event = method.parameters[1].type.classifier as KClass<out Event>
-                val handler = method.findAnnotation<EventHandler>()!!
-                val targetEntity = method.findAnnotation<TargetEntity>()
+        listener::class.members
+            .asSequence()
+            .filter { it.hasAnnotation<EventHandler>() }
+            .forEach { member ->
+                val event = member.parameters[1].type.classifier as KClass<out Event>
+                val handler = member.findAnnotation<EventHandler>()!!
+                val target = member.findAnnotation<TargetEntity>()
 
-                val provider = if (targetEntity != null) {
-                    providers.getOrPut(targetEntity.value) {
-                        targetEntity.value.objectInstance ?: targetEntity.value.java.getDeclaredConstructor().newInstance()
-                    }
-                } else {
-                    object : EntityProvider<Event> {
-                        override fun getTarget(event: Event): Entity? {
-                            return when {
-                                event is EntityEvent && event.entity == entity -> event.entity
-                                else -> null
-                            }
-                        }
-                    }
-                }
+                val provider = getOrCreateProvider(target, entity)
 
                 val registeredHandler = RegisteredEntityHandler(
                     listener,
                     handler.priority,
                     EventEntityProvider(event, provider)
-                ) { event -> method.call(listener, event) }
+                ) { event -> member.call(listener, event) }
 
                 handlers.getOrPut(event) { EntityHandlerList() }.register(registeredHandler)
 
-                plugin.server.pluginManager.registerEvent(
-                    event.java,
-                    listener,
-                    handler.priority,
-                    { _, event -> callEvent(event) },
-                    plugin,
-                    handler.ignoreCancelled
-                )
+                registerEvent(event, listener, handler)
             }
 
-        listeners.getOrPut(entity) { mutableSetOf() }.add(listener)
+        // Maybe thread-safe?
+        listeners.getOrPut(entity) { ConcurrentHashMap.newKeySet() }.add(listener)
+    }
+
+    private fun getOrCreateProvider(targetEntity: TargetEntity?, entity: Entity): EntityProvider<*> {
+        return if (targetEntity != null)
+            providers.getOrPut(targetEntity.value) {
+                targetEntity.value.objectInstance ?: targetEntity.value.java.getDeclaredConstructor().newInstance()
+            }
+        else
+            _providers.getOrPut(entity) {
+                object : EntityProvider<Event> {
+                    override fun getTarget(event: Event) = when (event) {
+                        is EntityEvent -> if (event.entity == entity) event.entity else null
+                        else -> null
+                    }
+                }
+            }
+    }
+
+    private fun registerEvent(event: KClass<out Event>, listener: Listener, handler: EventHandler) {
+        plugin.server.pluginManager.registerEvent(
+            event.java,
+            listener,
+            handler.priority,
+            { _, event -> callEvent(event) },
+            plugin,
+            handler.ignoreCancelled
+        )
     }
 
     /**
@@ -87,22 +99,25 @@ class EntityEventManager(private val plugin: Plugin) {
      * @param entity 대상
      * @param listener 이벤트리스너
      */
-    @Suppress("UNCHECKED_CAST")
     fun unregisterEvents(entity: Entity, listener: Listener) {
         listeners[entity]?.remove(listener)
 
         handlers.values.forEach { handlers ->
-            handlers.unregister { handler ->
-                handler.listener == listener &&
-                        (handler.provider.provider as? EntityProvider<Event>)?.getTarget(
-                            handler.provider.event.java.getDeclaredConstructor().newInstance()
-                        ) == entity
-            }
+            handlers.unregister { it.listener == listener && isTargetEntity(it, entity) }
         }
 
-        if (listeners[entity]?.isEmpty() == true) {
-            listeners.remove(entity)
-        }
+        if (listeners[entity]?.isEmpty() != true)
+            return
+
+        listeners.remove(entity)
+        _providers.remove(entity)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun isTargetEntity(handler: RegisteredEntityHandler, entity: Entity): Boolean {
+        return (handler.provider.provider as? EntityProvider<Event>)?.getTarget(
+            handler.provider.event.java.getDeclaredConstructor().newInstance()
+        ) == entity
     }
 
     /**
@@ -111,10 +126,10 @@ class EntityEventManager(private val plugin: Plugin) {
      * @param entity 대상
      */
     fun unregisterAllEvents(entity: Entity) {
-        listeners[entity]?.forEach { listener ->
-            unregisterEvents(entity, listener)
-        }
+        listeners[entity]?.forEach { unregisterEvents(entity, it) }
         listeners.remove(entity)
+
+        _providers.remove(entity)
     }
 
     /**
@@ -123,9 +138,7 @@ class EntityEventManager(private val plugin: Plugin) {
      * @param listener 이벤트리스너
      */
     fun unregisterListener(listener: Listener) {
-        handlers.values.forEach { handlers ->
-            handlers.unregister { it.listener == listener }
-        }
+        handlers.values.forEach { it.unregister { handler -> handler.listener == listener } }
         listeners.values.forEach { it.remove(listener) }
     }
 
